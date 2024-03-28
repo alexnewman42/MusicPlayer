e4b5df3c36957f40eea9e05b48aa268923cab02a
root <alexnewman42@gmail.com>
2024-03-28 08:37:51 -0400

    Add even listener to playButton

--- script.js
+++ script.js
@@ -114,6 +114,12 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+playButton.addEventListener('click', ()=>{
+  if (!userData?.currentSong) {
+    playSong(userData?.song[0].id);
+  }
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
