17ece2fb880a66e50b15b555259b40025e5e5b7b
root <alexnewman42@gmail.com>
2024-04-01 08:10:51 -0400

    Finish playPreviousSong function

--- script.js
+++ script.js
@@ -115,7 +115,12 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () => {
-
+  if (userData?.currentSong === null) return;
+  else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex -1];
+    playSong(previousSong.id);
+  }
 };
 
 const renderSongs = (array) => {
