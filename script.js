c0ee58c23b5faa31e56f12d70801e3b58ea985bc
root <alexnewman42@gmail.com>
2024-04-01 07:38:46 -0400

    Create pauseSong function

--- script.js
+++ script.js
@@ -100,6 +100,8 @@ const playSong = (id) => {
 
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
+  playButton.classList.remove("playing");
+  audio.pause();
 }
 
 const renderSongs = (array) => {
