2b8b1467f783e3bec8d4d3c9d5e9bb853d0ef5e6
root <alexnewman42@gmail.com>
2024-04-03 08:54:12 -0400

    Finish setPlayerDisplay function

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
+  setPlayerDisplay();
   audio.play();
 };
 
@@ -126,7 +127,12 @@ const playPreviousSong = () => {
 };
 
 const setPlayerDisplay = () => {
-  
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 }
 
 
