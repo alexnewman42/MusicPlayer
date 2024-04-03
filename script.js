950d1d4492e70993fdbbaeae70a5016a4018f294
root <alexnewman42@gmail.com>
2024-04-03 09:09:49 -0400

    Finish setPlayButtonAccessibleText function

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
 
   highlightCurrentSong();
   setPlayerDisplay();
+  setPlayButtonAccessibleText();
   audio.play();
 };
 
@@ -164,7 +165,8 @@ const renderSongs = (array) => {
 };
 
 const setPlayButtonAccessibleText = () => {
-
+  const song = userData?.currentSong || userData?.songs[0];
+  playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play");
 };
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
