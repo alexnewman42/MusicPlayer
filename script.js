5ea38bb9fc7a625e56aaf7bfb45c5475106bcb11
root <alexnewman42@gmail.com>
2024-04-05 08:12:35 -0400

    Finish shuffle function

--- script.js
+++ script.js
@@ -127,7 +127,15 @@ const playPreviousSong = () => {
   }
 };
 
-const shuffle = () => {};
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+};
 
 
 const setPlayerDisplay = () => {
