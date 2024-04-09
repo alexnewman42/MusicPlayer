bf0465a8a24cd9c30452895df2be9af603ceb0b2
root <alexnewman42@gmail.com>
2024-04-09 11:04:32 -0400

    Finish app

--- script.js
+++ script.js
@@ -220,6 +220,20 @@ pauseButton.addEventListener("click", pauseSong);
 nextButton.addEventListener('click', playNextSong);
 previousButton.addEventListener('click', playPreviousSong);
 shuffleButton.addEventListener('click', shuffle);
+audio.addEventListener('ended', () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = !(userData.songs.legth -1) > currentSongIndex;
+  if (nextSongExists) {
+    playNextSong(); 
+  } else {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
+  }
+});
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
