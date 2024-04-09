ce380633737dc3013bc0af82f6e221f445a6ccad
root <alexnewman42@gmail.com>
2024-04-09 10:20:45 -0400

    Add event listener to reset button

--- script.js
+++ script.js
@@ -151,6 +151,11 @@ const deleteSong = (id) => {
   if (userData?.songs.length === 0) {
     const resetButton = document.createElement('button');
     const resetText = document.createTextNode('Reset Playlist');
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset Playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+    resetButton.addEventListener('click', () => {});
   }
 };
 
